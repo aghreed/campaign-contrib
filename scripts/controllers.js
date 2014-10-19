f7eddf0e4c23325ae4fb89d2e8862cd3ae6948a8
Alex Reed <alex.reed@aviture.us.com>
2014-10-19 12:55:28 -0500

    gulp watch:less task - insert backdrop image - add in dummy json - menu items active

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -4,8 +4,18 @@
   var app = angular.module('campaign-contrib');
 
     // candidates
-    app.controller('CandidatesController', ['$scope', '$log', function ($scope, $log) {
+    app.controller('CandidatesController', ['$scope', '$http', '$log', function ($scope, $http, $log) {
       $log.info('Candidate Controller Reached');
+
+      $http({method: 'GET', url: 'data/test.json'})
+        .success(function(data) {
+          $scope.testItems = data.test;
+          $log.info($scope.test);
+        }).error(function (error) {
+          $log.error(error);
+        });
+
+        
     }]);
 
     // contributors
